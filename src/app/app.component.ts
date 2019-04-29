57d9a26a2adcc9d71ef35392ea6344c988e48766
Tiago de Jesus Sousa Gama <tiago.gama@cartaobrb.com.br>
2019-04-29 11:06:22 -0300

    projeto firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { Firebase } from '@ionic-native/firebase/ngx';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -12,9 +14,11 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private firebase: Firebase
   ) {
     this.initializeApp();
+    this.solicitarTokenDoFirebase();
   }
 
   initializeApp() {
@@ -22,5 +26,32 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  solicitarTokenDoFirebase() {
+ 
+    this.firebase.getToken()
+      .then(token => {
+        console.log("firebase token recebido", token);
+        this.enviarTokenParaOservidor(token);
+        this.iniciarListenerDeNotificacoes();
+      }) // save the token server-side and use it to push notifications to this device
+      .catch(error => {
+        console.error('Error getting token', error)
+      });
+ 
+  }
+ 
+  iniciarListenerDeNotificacoes() {
+ 
+    this.firebase.onNotificationOpen().subscribe((notification: any) => {
+      console.log(notification);
+    })
+ 
+  }
+ 
+  enviarTokenParaOservidor(token) {
+ 
+    //lógica para enviar o token para o seu servidor através da sua api
   }
 }
